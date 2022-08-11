9e4e8a8408697aadf4af97d071a61a3f0b714f89
carmeloferso <111031067+carmeloferso@users.noreply.github.com>
2022-08-10 22:06:52 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println(sumar(10,20,30));
+
+        Coche miCoche = new Coche();
+        miCoche.incrementarPuerta();
+        miCoche.incrementarPuerta();
+        System.out.println(miCoche.nroPuertas);
+        System.out.println();
+    }
+
+    public static int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+class Coche {
+    int nroPuertas = 4;
+
+    public void incrementarPuerta(){
+        this.nroPuertas++;
+    }
+}
